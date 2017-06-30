424901457c77f89535602eda14cdbcb6833feda8
SephirothGCB <guillecuesta@gmail.com>
2017-06-30 17:06:04 +0200

    1. The app is ready for using Kotlin. 2. SplashActivity has been added to the project.

---
+++ droid/app/src/main/java/com/r15k/glacialware/r15k/views/splash/SplashActivity.kt
@@ -0,0 +1,16 @@
+package com.r15k.glacialware.r15k.views.splash
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.r15k.glacialware.r15k.R
+
+/**
+ * Created by Guille on 30/06/2017.
+ */
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        this.setContentView(R.layout.activity_splash)
+    }
+}
