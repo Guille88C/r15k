71b58a26c4adaa5d46000bf75ad16315a9f92833
SephirothGCB <guillecuesta@gmail.com>
2017-07-01 12:57:02 +0200

    1. Splash activity has changed, now is done with a good practice code. 2. The transition between splash activity and main activity is done.

---
+++ droid/app/src/main/java/com/r15k/glacialware/r15k/views/main/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.r15k.glacialware.r15k.views.main
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Guille on 01/07/2017.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        this.setContentView(com.r15k.glacialware.r15k.R.layout.activity_main)
+    }
+
+}
--- droid/app/src/main/java/com/r15k/glacialware/r15k/views/splash/SplashActivity.kt
+++ droid/app/src/main/java/com/r15k/glacialware/r15k/views/splash/SplashActivity.kt
@@ -1,8 +1,9 @@
 package com.r15k.glacialware.r15k.views.splash
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.r15k.glacialware.r15k.R
+import com.r15k.glacialware.r15k.views.main.MainActivity
 
 /**
  * Created by Guille on 30/06/2017.
@@ -11,6 +12,7 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        this.setContentView(R.layout.activity_splash)
+        var intent = Intent(this, MainActivity :: class.java)
+        this.startActivity(intent)
     }
 }
