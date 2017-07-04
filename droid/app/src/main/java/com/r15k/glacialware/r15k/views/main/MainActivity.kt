b0e5da931013902260b57f36523b987bf03234d9
Tanuki <phoe88@gmail.com>
2017-07-04 17:52:50 +0200

    Kotlin extension added

--- droid/app/src/main/java/com/r15k/glacialware/r15k/views/main/MainActivity.kt
+++ droid/app/src/main/java/com/r15k/glacialware/r15k/views/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.r15k.glacialware.r15k.views.main
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * Created by Guille on 01/07/2017.
@@ -12,6 +13,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         this.setContentView(com.r15k.glacialware.r15k.R.layout.activity_main)
+
+        textView.text = "your mother is a fucker"
+
     }
 
 }
