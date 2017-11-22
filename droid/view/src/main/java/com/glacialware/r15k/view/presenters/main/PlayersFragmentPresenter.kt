45393f28188cf67fe4f4ccdcf089f322a0ecf4d4
SephirothGCB <guillecuesta@gmail.com>
2017-11-22 16:18:15 +0100

    1. I have modified the presenter from PayersFragment view. It simlifies the view.

---
+++ droid/view/src/main/java/com/glacialware/r15k/view/presenters/main/PlayersFragmentPresenter.kt
@@ -0,0 +1,66 @@
+package com.glacialware.r15k.view.presenters.main
+
+import android.arch.lifecycle.ViewModelProviders
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.glacialware.r15k.model.room.Player
+import com.glacialware.r15k.view.R
+import com.glacialware.r15k.view.presenters.GenericFragmentPresenter
+import com.glacialware.r15k.view.views.main.PlayersAdapter
+import com.glacialware.r15k.view.views.main.PlayersFragment
+import com.glacialware.r15k.view.wireframes.main.PlayersWireframe
+import com.glacialware.r15k.view.wireframes.main.PlayersWireframeImpl
+import com.glacialware.r15k.viewmodel.views.main.PlayersViewModel
+import kotlinx.android.synthetic.main.fragment_players.*
+
+/**
+ * Created by Guille on 16/11/2017.
+ */
+class PlayersFragmentPresenter(private val view : PlayersFragment) : GenericFragmentPresenter() {
+
+    private var viewModel: PlayersViewModel? = null
+    private val wireframe : PlayersWireframe = PlayersWireframeImpl(view)
+    private var mPlayersAdapter : PlayersAdapter? = null
+
+    override fun initViewModel(inflater: LayoutInflater, container: ViewGroup): View? {
+        this.viewModel = ViewModelProviders.of(view.activity).get(PlayersViewModel::class.java)
+        if (this.viewModel != null) {
+            view.lifecycle.addObserver(this.viewModel!!)
+        }
+        return inflater.inflate(R.layout.fragment_players, container, false)
+    }
+
+    fun onItemPlayerClick(player: Player) {
+        wireframe.goToPlayerDetail(player)
+    }
+
+    fun initPlayersObserver() {
+        if (this.viewModel != null && this.viewModel?.lPlayers != null) {
+            this.viewModel?.lPlayers?.observe(
+                {
+                    view.lifecycle
+                },
+                { lItems ->
+                    if (lItems != null) {
+                        this.initPlayers(lItems)
+                    }
+                }
+            )
+        }
+    }
+
+    private fun initPlayers(lItems : List<Player>) {
+        if (mPlayersAdapter == null) {
+            view.rvPlayers.layoutManager = LinearLayoutManager(view.activity)
+            this.mPlayersAdapter = PlayersAdapter(lItems as MutableList<Player>)
+            view.rvPlayers.adapter = this.mPlayersAdapter
+
+            this.mPlayersAdapter?.itemClick = view
+        }
+        else {
+            mPlayersAdapter?.update(lItems)
+        }
+    }
+}
--- droid/view/src/main/java/com/glacialware/r15k/view/views/main/PlayersFragment.kt
+++ droid/view/src/main/java/com/glacialware/r15k/view/views/main/PlayersFragment.kt
@@ -1,23 +1,15 @@
 package com.glacialware.r15k.view.views.main
 
-import android.arch.lifecycle.*
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.support.v7.widget.LinearLayoutManager
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.glacialware.r15k.model.room.Player
-import com.glacialware.r15k.view.R
-import com.glacialware.r15k.view.presenters.main.PlayersPresenter
-import com.glacialware.r15k.view.presenters.main.PlayersPresenterImpl
-import com.glacialware.r15k.viewmodel.views.main.PlayersViewModel
-import kotlinx.android.synthetic.main.fragment_players.*
+import com.glacialware.r15k.view.presenters.main.PlayersFragmentPresenter
+import com.glacialware.r15k.view.views.generic.GenericRootFragment
 
 /**
  * Created by Guille on 09/07/2017.
  */
-class PlayersFragment : Fragment(), IPlayerClick {
+class PlayersFragment : GenericRootFragment, IPlayerClick {
     companion object {
         @JvmStatic
         val TAG : String = this :: class.java.canonicalName
@@ -31,63 +23,21 @@ class PlayersFragment : Fragment(), IPlayerClick {
         }
     }
 
-    private lateinit var presenter : PlayersPresenter
-    private lateinit var mPlayersVM : PlayersViewModel
-    private var mPlayersAdapter : PlayersAdapter? = null
-
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // view model
-        this.mPlayersVM = ViewModelProviders.of(this).get(PlayersViewModel::class.java)
-        this.lifecycle.addObserver(this.mPlayersVM)
-        // ----
-
-        return inflater?.inflate(R.layout.fragment_players, container, false)
+    constructor() : super() {
+        this.presenter = PlayersFragmentPresenter(this)
     }
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        presenter = PlayersPresenterImpl(this)
-        this.initPlayersObserver()
-    }
-
-    fun initPlayersObserver() {
-
-        this.mPlayersVM.lPlayers.observe(
-                {
-                    this.lifecycle
-                },
-                {
-                    lItems ->
-                    if (lItems != null) {
-                        this.initPlayers(lItems)
-                    }
-                }
-        )
-
-//        this.mPlayersVM.ListPlayers.observe(this, Observer<ArrayList<Player>> { lItemsObserver ->
-//            if (lItemsObserver != null) {
-//                this.initPlayers(lItemsObserver)
-//            }
-//        })
-    }
-
-    fun initPlayers(lItems : List<Player>) {
-        if (mPlayersAdapter == null) {
-            this.rvPlayers.layoutManager = LinearLayoutManager(this.activity)
-            this.mPlayersAdapter = PlayersAdapter(lItems as MutableList<Player>)
-            this.rvPlayers.adapter = this.mPlayersAdapter
-
-            this.mPlayersAdapter?.itemClick = this
-        }
-        else {
-            mPlayersAdapter?.update(lItems)
+        if (this.presenter != null && this.presenter is PlayersFragmentPresenter) {
+            (this.presenter as PlayersFragmentPresenter).initPlayersObserver()
         }
     }
 
     override fun onPlayerClick(item: Player) {
-        if (presenter != null) {
-            presenter.onItemPlayerClick(item)
+        if (this.presenter != null && this.presenter is PlayersFragmentPresenter) {
+            (this.presenter as PlayersFragmentPresenter).onItemPlayerClick(item)
         }
     }
 }
