5f6efe6ebcd5e5bde76fb2cca5b822661a928eee
SephirothGCB <thecrow88@gmail.com>
2018-03-30 18:47:43 +0200

    Wireframe is injected with dagger in Splash view.

--- droid/view/src/main/java/com/glacialware/r15k/view/views/splash/SplashActivity.kt
+++ droid/view/src/main/java/com/glacialware/r15k/view/views/splash/SplashActivity.kt
@@ -2,20 +2,29 @@ package com.glacialware.r15k.view.views.splash
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.glacialware.r15k.view.RiskApplication
+import com.glacialware.r15k.view.views.di.ActivityModule
 import com.glacialware.r15k.view.wireframes.splash.SplashActivityWireframe
+import javax.inject.Inject
 
 /**
 * Created by Guille on 30/06/2017.
 */
 class SplashActivity : AppCompatActivity() {
 
+    // ---- Dagger attributes ----
+    @field:[Inject]
+    lateinit var mWireframe: SplashActivityWireframe
+    // ---- END Dagger attributes ----
+
     // ---- Activity ----
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val wireframe = SplashActivityWireframe(this)
-        wireframe.goToMain()
+        (application as RiskApplication).applicationComponent.with(ActivityModule(this)).inject(this)
+
+        mWireframe.goToMain()
         finish()
     }
 
